9d8ee2938ed90c46301c9a8948cfd3aadda2036c
Ryan Grenz <info@ryangrenz.com>
2014-02-18 11:54:43 +0000

    Initial commit of changes required for an ssh feature. This is by no means a complete change.
    More error checking, structural work and test cases to follow.
    
    THIS SOFTWARE CONTRIBUTION IS PROVIDED BY THE CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED

---
+++ src/cf/api/app_ssh.go
@@ -0,0 +1,62 @@
+package api
+
+import (
+	"cf/configuration"
+	"cf/net"
+	"fmt"
+//	"io/ioutil"
+//	"path"
+//	"os/exec"
+//	"strconv"
+//   "os"
+	"cf/models"
+)
+
+
+type AppSshRepository interface {
+	GetSshDetails(appGuid string, instance int) (apiResponse net.ApiResponse, sshDetails models.SshConnectionDetails)
+}
+
+
+
+type CloudControllerAppSshRepository struct {
+	config			configuration.Reader
+	gateway 		net.Gateway
+}
+
+func NewCloudControllerAppSshRepository(config configuration.Reader, gateway net.Gateway) (repo CloudControllerAppSshRepository) {
+	repo.config = config
+	repo.gateway = gateway
+	return
+}
+
+
+func (repo CloudControllerAppSshRepository) GetSshDetails(appGuid string, instance int) (apiResponse net.ApiResponse, sshDetails models.SshConnectionDetails) {
+
+	url := fmt.Sprintf("%s/v2/apps/%s/instances/%d/ssh", repo.config.ApiEndpoint(), appGuid, instance)
+	request, apiResponse := repo.gateway.NewRequest("GET", url, repo.config.AccessToken(), nil)
+	
+	if apiResponse.IsNotSuccessful() {
+		return
+	}
+
+	serverResponse := new(struct {
+		Ip            string `json:"ip"`
+		Port          int `"port"`
+		User          string `json:"user"`
+		SshKey        string `json:"sshkey"`
+	})
+	
+	_, apiResponse = repo.gateway.PerformRequestForJSONResponse(request, &serverResponse)
+	if apiResponse.IsNotSuccessful() {
+		return
+	}
+	
+	sshDetails.Ip=serverResponse.Ip
+	sshDetails.Port=serverResponse.Port
+	sshDetails.User=serverResponse.User
+	sshDetails.SshKey=serverResponse.SshKey
+	
+	return
+}
+
---
+++ src/cf/models/ssh.go
@@ -0,0 +1,8 @@
+package models
+
+type SshConnectionDetails struct {
+	Ip            string
+	Port          int
+	User          string
+	SshKey        string
+}
